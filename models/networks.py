fix normalization error

models/networks.py
@@ -201,12 +201,13 @@ class AuFCN(nn.Module):
 
     def update(self, my_dict, sample, dict_index, low):
         new = sample.view(-1, sample.shape[2])
-        if low:
-            normalized = new / torch.pow(torch.sum(new * new, 1, keepdim=True), 0.5)
-        else:
-            normalized = new
-        my_dict[dict_index:dict_index+normalized.shape[0], :] = normalized
-        return dict_index + normalized.shape[0]
+#        if low:
+#            normalized = new / torch.pow(torch.sum(new * new, 1, keepdim=True), 0.5)
+#        else:
+#            normalized = new
+#        my_dict[dict_index:dict_index+normalized.shape[0], :] = normalized
+        my_dict[dict_index:dict_index+new.shape[0], :] = new
+        return dict_index + new.shape[0]
 
     def forward(self, sample):
         degraded = sample[0]